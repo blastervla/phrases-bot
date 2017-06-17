cfacb98d26fc551ab1181958244c0f73fc0c867c
Blastervla <blastervla@gmail.com>
2017-06-17 16:18:36 -0300

    Fix webhook

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const TeleBot = require('telebot');
+var express = require('express');
+var app = express();
 var Credentials = require('./Credentials.js');
-const bot = new TeleBot(Credentials.getAuthToken());
-// On inline query
+const bot = new TeleBot({
+    token: Credentials.getAuthToken(), // Required. Telegram Bot API token.
+    polling: { // Optional. Use polling.
+        interval: 1000, // Optional. How often check updates (in ms).
+        limit: 100, // Optional. Limits the number of updates to be retrieved.
+        retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
+    }
+});
 bot.on('inlineQuery', msg => {
     let query = msg.query;
-    console.log(`inline query: ${ query }`);
 
     // Create a new answer list object
     const answers = bot.answerList(msg.id, {cacheTime: 60});
@@ -37,3 +44,7 @@ bot.on('inlineQuery', msg => {
 });
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 bot.start();
+// Finally, start our server
+app.listen(3000, function() {
+  console.log('Telegram app listening on port 3000!');
+});
