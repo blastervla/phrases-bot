94599e2ae2ef2c9d7c370d79c83f14995761a0c0
Blastervla <blastervla@gmail.com>
2017-06-17 15:42:43 -0300

    Fix webhook

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const TeleBot = require('telebot');
 var Credentials = require('./Credentials.js');
-const bot = new TeleBot({
-    token: 'bot' + Credentials.getAuthToken(),
-    webhook: {
-        url: 'https://api.telegram.org',
-        host: 'localhost',
-        port: (process.env.PORT || 5000)
-    }
-});
-
+const bot = new TeleBot(Credentials.getAuthToken());
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 bot.start();
