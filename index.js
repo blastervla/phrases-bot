bdfab1cc90ae3ba1333e58faeacb3f7fd9d53c47
Blastervla <blastervla@gmail.com>
2017-06-17 14:44:51 -0300

    Fix requirements

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TeleBot = require('telebot');
-var Credentials = require('../Credentials.js');
+var Credentials = require('./Credentials.js');
 const bot = new TeleBot({
     token: Credentials.getAuthToken(),
     webhook: {
