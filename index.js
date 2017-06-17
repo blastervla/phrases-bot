d17ed91d38fec2579bcda1eae4bb669f8ead7658
Blastervla <blastervla@gmail.com>
2017-06-17 15:22:44 -0300

    Fix webhook

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var Credentials = require('./Credentials.js');
 const bot = new TeleBot({
     token: Credentials.getAuthToken(),
     webhook: {
-        url: 'https://api.telegram.org/bot' + Credentials.getAuthToken() + '/sendMessage',
+        url: 'https://phrases-bot.herokuapp.com/new-message',
         host: 'localhost',
         port: (process.env.PORT || 5000)
     }
 });
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
-
 bot.start();
