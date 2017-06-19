36ea7a04aa408679ed12fee974e4caf503bc46fa
Blastervla <blastervla@gmail.com>
2017-06-19 16:08:49 -0300

    Test

--- DropboxManager.js
+++ DropboxManager.js
@@ -2,17 +2,14 @@ var Dropbox = require('dropbox');
 var Credentials = require('./Credentials.js');
 function getFileLink(file) {
 	var dbx = new Dropbox({ accessToken: Credentials.getDropboxAuthToken() });
-	/*dbx.sharingCreateSharedLinkWithSettings({path: '/Phrases-Bot/TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'}).then(function(response) {
-      	console.log(response);
-    });*/
-    dbx.sharingGetSharedLinks({path: '/Phrases-Bot/TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'}).then(function(response) {
+	dbx.sharingCreateSharedLinkWithSettings({path: '/Phrases-Bot/TelegramAudios/prueba2.ogg'}).then(function(response) {
       	console.log(response);
       	console.log(response.links);
       	console.log(response.links[0].url);
-      	console.log('Index: ' + response.indexOf('https://www.dropbox.com/s/'));
-      	console.log('Index2: ' + response.indexOf('?dl=0'));
-      	console.log(response.substring(response.indexOf('https://www.dropbox.com/s/'), response.indexOf('?dl=0')));
     });
+    /*dbx.sharingGetSharedLinks({path: '/Phrases-Bot/TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'}).then(function(response) {
+      	return response.links[0].url;
+    });*/
 }
 module.exports = {
 	getFileLink: getFileLink
