a334b3f8e850630e38e58744008ef2fedcd81c1c
Blastervla <blastervla@gmail.com>
2017-06-19 14:31:34 -0300

    Test

--- DropboxManager.js
+++ DropboxManager.js
@@ -2,11 +2,7 @@ var Dropbox = require('dropbox');
 var Credentials = require('./Credentials.js');
 function getFileLink(file) {
 	var dbx = new Dropbox({ accessToken: Credentials.getDropboxAuthToken() });
-	dbx.sharingCreateSharedLinkWithSettings({path: '/Phrases-Bot/TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'}).then(function(response) {
-		console.log('Dropbox test');
-		console.log(response.url);
-		console.log(response);
-	});
+	console.log(dbx.sharingCreateSharedLinkWithSettings({path: '/Phrases-Bot/TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'}));
 }
 module.exports = {
 	getFileLink: getFileLink
