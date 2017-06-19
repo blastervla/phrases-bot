9f174c728422b93cf7e4b42640fb612ae4327422
Blastervla <blastervla@gmail.com>
2017-06-19 12:57:40 -0300

    Test

--- Credentials.js
+++ Credentials.js
@@ -1,7 +1,12 @@
 var authToken = "422907398:AAG1dQbHkh2tmK39TJD_TAQeY5XDEn9au6s";
+var dropBoxAuthToken = "pBNY_4nhfkwAAAAAAAAAQB4cnhmOqGeeNLC_Ra4vBaBr80ja9ReOwnRPR3JMQ2xG";
 function getAuthToken() {
 	return authToken;
 }
+function getDropboxAuthToken() {
+	return dropBoxAuthToken;
+}
 module.exports = {
-	getAuthToken: getAuthToken
+	getAuthToken: getAuthToken,
+	getDropboxAuthToken: getDropboxAuthToken
 };
---
+++ DropboxManager.js
@@ -0,0 +1,8 @@
+var Dropbox = require('dropbox');
+var Credentials = require('./Credentials.js');
+function getFileLink(file) {
+	console.log(Dropbox.sharingCreateSharedLinkWithSettings(new SharingCreateSharedLinkWithSettingsArg('TelegramAudios/Nunca_compres_tiempos_compartidos.ogg')).url);
+}
+module.exports = {
+	getFileLink: getFileLink
+};
