50322f26bd158147a9228bfe20fff97456e11aa3
Blastervla <blastervla@gmail.com>
2017-06-19 13:09:55 -0300

    Test

--- DropboxManager.js
+++ DropboxManager.js
@@ -1,7 +1,7 @@
 var Dropbox = require('dropbox');
 var Credentials = require('./Credentials.js');
 function getFileLink(file) {
-	var prefs.path = 'TelegramAudios/Nunca_compres_tiempos_compartidos.ogg';
+	var prefs = {path: 'TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'};
 	console.log(Dropbox.sharingCreateSharedLinkWithSettings(prefs).url);
 }
 module.exports = {
