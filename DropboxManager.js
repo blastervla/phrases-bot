468cf7573c7a9f04323dadda366dbfdcdb21863a
Blastervla <blastervla@gmail.com>
2017-06-19 16:12:04 -0300

    Test

--- DropboxManager.js
+++ DropboxManager.js
@@ -4,8 +4,8 @@ function getFileLink(file) {
 	var dbx = new Dropbox({ accessToken: Credentials.getDropboxAuthToken() });
 	dbx.sharingCreateSharedLinkWithSettings({path: '/Phrases-Bot/TelegramAudios/prueba2.ogg'}).then(function(response) {
       	console.log(response);
-      	console.log(response.links);
-      	console.log(response.links[0].url);
+      	console.log(response.url);
+      	console.log(response[0].url);
     });
     /*dbx.sharingGetSharedLinks({path: '/Phrases-Bot/TelegramAudios/Nunca_compres_tiempos_compartidos.ogg'}).then(function(response) {
       	return response.links[0].url;
