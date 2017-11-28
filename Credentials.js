e1835408fd7682f09724bc5bfb5bc7d7c7bd3cf5
Vladimir Pomsztein <vladimir.pomsztein@mercadolibre.com>
2017-11-28 10:32:27 -0300

    Create mirror

--- Credentials.js
+++ Credentials.js
@@ -1,4 +1,4 @@
-var authToken = "422907398:AAG1dQbHkh2tmK39TJD_TAQeY5XDEn9au6s";
+var authToken = "505043091:AAGqJ-sJffzjJwIfpVSYySPyaWLsMgjuEF0";
 var dropBoxAuthToken = "pBNY_4nhfkwAAAAAAAAAQpvSsGfIDc8UUi1lVKJfqyoNyO8zbYnQ3cRvglgDAtj1";
 function getAuthToken() {
 	return authToken;
